34e0da9f0c86af386ac668ed7a65dc6582b7fd66
yousra <yousra@yousras-mbp.home>
2025-09-01 03:05:08 +0200

    add link

--- server/controles/restpassword.ts
+++ server/controles/restpassword.ts
@@ -5,7 +5,7 @@ export async function resetPassword(req: Request, res: Response) {
   const { email } = req.body;
   if (!email) return res.status(400).json({ error: "Email is required" });
 
-  const { error } = await supabase.auth.resetPasswordForEmail(email,{ redirectTo: "http://localhost:3000/updatepassword" });
+  const { error } = await supabase.auth.resetPasswordForEmail(email,{ redirectTo: "https://yousrabank.onrender.com/updatepassword" });
 
   if (error) return res.status(400).json({ error: error.message });
   res.json({ message: "Password reset email sent" });
