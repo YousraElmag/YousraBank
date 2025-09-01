ae20f0d27d9a5c42b7b542936d6e0cb0e4c662c1
yousra <yousra@yousras-mbp.home>
2025-09-01 02:54:49 +0200

    add render link

--- client/src/app/restpassword/page.tsx
+++ client/src/app/restpassword/page.tsx
@@ -9,7 +9,7 @@ export default function ResetPassword() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/reset-password", {
+      const res = await fetch("https://yousrabank.onrender.com/api/auth/reset-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
