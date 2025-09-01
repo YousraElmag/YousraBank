07fe7847f27d26cea237faa51bdb395b522964c0
yousra <yousra@yousras-mbp.home>
2025-09-01 13:00:07 +0200

    remove git history

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -32,7 +32,7 @@ const router=useRouter()
     try {
     
 
-      const res = await fetch("https://yousrabank.onrender.com/api/auth/register", {
+      const res = await fetch("http://localhost:5000/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password,firstname,lastname}),
