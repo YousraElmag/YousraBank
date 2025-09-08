db7fea56b8d7fc03300ac850ac809c4b999b7de1
yousra <yousra@yousras-mbp.home>
2025-09-08 10:55:42 +0200

    ad

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -29,7 +29,7 @@ export default function Register() {
 
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`,
+        `/api/auth/register`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
