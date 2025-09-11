17de3b573cbb44318bd8616cbc6dfb4b81baf1b8
yousra <yousra@yousras-mbp.home>
2025-09-11 11:03:51 +0200

    add

--- server/server.ts
+++ server/server.ts
@@ -5,8 +5,17 @@ import authRoutes from "./routes/auth";
 
 import cors from "cors";
 const app = express();
-app.use(cors());
+
 app.use(express.json());
+app.use(cors({
+  origin: [
+    "https://yousra-bank-mz2y14qc7-yousraelmags-projects.vercel.app", 
+    "http://localhost:3000" 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
 app.use("/api/auth", authRoutes);
 
